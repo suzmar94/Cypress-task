3c11e9502e85b1b660791fdf33a4fcc09ddcdcea
sma <suzana.markovic@p3-group.com>
2022-02-21 17:21:23 +0100

    Uploading latest changes

---
+++ CypressJavascript/cypress/integration/First_Try/first-test.js
@@ -0,0 +1,30 @@
+///<reference types="cypress"/>
+
+describe('Verify Browser Stack Home Page', () => {
+
+    it('Verify SwagLabs logo is visible', () => {
+
+        cy.openHomePage();
+        cy.get('.bot_column').should('be.visible');
+        cy.get('[placeholder="Username"]').type('standard_user');
+        cy.get('[placeholder="Password"]').type('secret_sauce');
+        cy.get('#login-button').click();
+        cy.get('.product_sort_container').select(2);
+        // cy.get('product_sort_container').contains('Price (low to high)');
+        cy.get('#add-to-cart-sauce-labs-onesie').click();
+        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click();
+        cy.get('#shopping_cart_container').click();
+        cy.get('#checkout').click();
+        cy.get('[placeholder="First Name"]').type('Suzana');
+        cy.get('[placeholder="Last Name"]').type('Markovic');
+        cy.get('[placeholder="Zip/Postal Code"]').type('11030');
+        cy.get('#continue').click();
+        cy.get('#finish').click();
+        cy.get('#back-to-products').click();
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#logout_sidebar_link').click();
+        
+    })
+
+
+})
